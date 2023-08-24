5459fd03ff391360fd310b0963091929b66a53af
med <moufidmed2@gmail.com>
2023-08-24 14:51:33 +0100

    the last tch

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,105 @@
 import Stick from "./components/stick";
 import Ball from "./components/ball";
 import { useEffect, useState } from "react";
-import styles from './App.module.css'
-
+import styles from "./App.module.css";
 
 const randomDigit = () => (Math.ceil(Math.random() * 10) % 2 === 0 ? 1 : -1);
 
+const ai = () => {};
 
-const ai = () => {
-
-}
-
-const App = () => { 
-  
+const App = () => {
   const [position1, setPosition1] = useState({ top: 0, left: 5 });
-  const [position2, setPosition2] = useState({ top: window.innerHeight/2, left: window.innerWidth - 15});
-  const [ballPosition, setBallPosition] = useState({ top: window.innerHeight/2, left: window.innerWidth/2 });
-  const [coordinates,setCoordinates] = useState({x:1,y:1})
-  const [ballSpeed,setBallSpeed] = useState(2);
-  const [aiSpeed,setAiSpeed] = useState(1)
+  const [position2, setPosition2] = useState({
+    top: window.innerHeight / 2,
+    left: window.innerWidth - 15,
+  });
+  const [ballPosition, setBallPosition] = useState({
+    top: window.innerHeight / 2,
+    left: window.innerWidth / 2,
+  });
+  const [coordinates, setCoordinates] = useState({ x: 1, y: 1 });
+  const [ballSpeed, setBallSpeed] = useState(2);
+  const [aiSpeed, setAiSpeed] = useState(1);
 
-  // speedUp until the the stick get the same coordinates of the ball 
+  // speedUp until the the stick get the same coordinates of the ball
   const slowDown = (
-    coordinates:{x:number,y:number},
-    position2:{top:number,left:number},
-    setPosition2:React.Dispatch<React.SetStateAction<{top:number,left:number}>>,
-    ballSpeed:number,
-    aiSpeed:number,
+    coordinates: { x: number; y: number },
+    position2: { top: number; left: number },
+    setPosition2: React.Dispatch<
+      React.SetStateAction<{ top: number; left: number }>
+    >,
+    ballSpeed: number,
+    aiSpeed: number
   ) => {
-    if(coordinates.x > 0)
-    {
-      let bottom  = position2.top + 140;
-      if(ballPosition.top >= (bottom - 30))
-      setPosition2((previous)=>({top:previous.top + ballSpeed,left:previous.left}))
-      else setPosition2((previous)=>({top:previous.top + aiSpeed,left:previous.left}))
+    if (coordinates.x > 0) {
+      let bottom = position2.top + 140;
+      if (ballPosition.top >= bottom - 30)
+        setPosition2((previous) => ({
+          top: previous.top + ballSpeed,
+          left: previous.left,
+        }));
+      else
+        setPosition2((previous) => ({
+          top: previous.top + aiSpeed,
+          left: previous.left,
+        }));
     }
-    if(coordinates.x < 0)
-    {
-      if(ballPosition.top <= position2.top)
-      setPosition2((previous)=>({top:previous.top - ballSpeed,left:previous.left}))
-      else setPosition2((previous)=>({top:previous.top - aiSpeed,left:previous.left}))      
+    if (coordinates.x < 0) {
+      if (ballPosition.top <= position2.top)
+        setPosition2((previous) => ({
+          top: previous.top - ballSpeed,
+          left: previous.left,
+        }));
+      else
+        setPosition2((previous) => ({
+          top: previous.top - aiSpeed,
+          left: previous.left,
+        }));
     }
+  };
 
-  }
+  useEffect(() => {
+    let inter = setInterval(() => {
+      // move the ball to the left later
+      setBallPosition((previous) => ({
+        top: previous.top + coordinates.x * ballSpeed,
+        left: previous.left,
+      }));
+    }, 1);
+    return () => clearInterval(inter);
+  }, [coordinates]);
 
-  useEffect(()=>{
-    
-    let inter = setInterval(()=>{
-        // move the ball to the left later 
-        setBallPosition((previous)=>({top:(previous.top + (coordinates.x * ballSpeed)),left : previous.left}))
-    },1)
-    return () => clearInterval(inter)
-  },[coordinates])
+  useEffect(() => {
+    slowDown(coordinates, position2, setPosition2, ballSpeed, aiSpeed);
+  }, [ballPosition]);
 
-  useEffect(()=>{
-   slowDown(coordinates,position2,setPosition2,ballSpeed,aiSpeed)
-    
-  },[ballPosition])
-
-  useEffect(()=>{
-      if(ballPosition.top <= 0 || ballPosition.top >= (window.innerHeight - 30))
-      {
-        setCoordinates((previous)=>({x:-1 * previous.x,y:previous.y}))
-        // setBallSpeed(ballSpeed + 1)
-      }
-  },[ballPosition])
-
-const handlMouseMove = (e:MouseEvent) => {
-      console.log(e.clientY)
-      if(e.clientY > 70 && e.clientY < window.innerHeight - 70)
-      setPosition1((previous)=>({top:e.clientY - 70,left:previous.left}))
-
+  useEffect(() => {
+    if (ballPosition.top <= 0 || ballPosition.top >= window.innerHeight - 30) {
+      setCoordinates((previous) => ({ x: -1 * previous.x, y: previous.y }));
+      // setBallSpeed(ballSpeed + 1)
     }
+  }, [ballPosition]);
 
-  useEffect(()=>{
-    window.addEventListener("mousemove",handlMouseMove)
-    return () => window.removeEventListener("mousemove",handlMouseMove)
-  },[ballPosition])
+  const handlMouseMove = (e: MouseEvent) => {
+    console.log(e.clientY);
+    if (e.clientY >= 70 && e.clientY <= window.innerHeight - 70)
+      setPosition1((previous) => ({
+        top: e.clientY - 70,
+        left: previous.left,
+      }));
+    else if (e.clientY < 70)
+      setPosition1((previous) => ({ top: 0, left: previous.left }));
+    else if (e.clientY > window.innerHeight - 70)
+      setPosition1((previous) => ({
+        top: window.innerHeight - 140,
+        left: previous.left,
+      }));
+  };
 
-
+  useEffect(() => {
+    window.addEventListener("mousemove", handlMouseMove);
+    return () => window.removeEventListener("mousemove", handlMouseMove);
+  }, [ballPosition]);
 
   return (
     <div className={styles["container"]}>
@@ -86,7 +107,7 @@ const handlMouseMove = (e:MouseEvent) => {
       <Ball position={ballPosition}></Ball>
       <Stick position={position2}></Stick>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
